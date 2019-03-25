a57e4935379113a9a340a68d36faf97240c4833f
NguyenLuongDuyKhanh <16520584@gm.uit.edu.vn>
2019-03-25 18:09:05 +0700

    Lab 1...

--- PJFile/HelloWorldBlink/main.c
+++ PJFile/HelloWorldBlink/main.c
@@ -6,7 +6,7 @@ void delay(int n);
 int main(void)  //Main program
 {
    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
-   P1DIR |= 11000001; // Set P1.0 to output and P1.3 to input direction
+   P1DIR |= 0b01000001; // Set P1.0 to output and P1.3 to input direction
 
    while(1) //Loop forever, we'll do our job in the interrupt routine...
    {
@@ -17,14 +17,14 @@ int main(void)  //Main program
        int B11=delayCircleNum;
        while(B11!=0)
        {
-       P1OUT |= 00000001;
+       P1OUT |= 0b00000001;
        B11--;
        }
 
        int B12=delayCircleNum;
        while(B12!=0)
        {
-       P1OUT &= 00000000;
+       P1OUT &= 0b00000000;
        B12--;
        }
        LoopB1--;
@@ -33,14 +33,14 @@ int main(void)  //Main program
        int B21=delayCircleNum;
        while(B21!=0)
        {
-       P1OUT |= 11000001;
+       P1OUT |= 0b01000001;
        B21--;
        }
 
        int B22=delayCircleNum;
        while(B22!=0)
        {
-        P1OUT &= 00000000;
+        P1OUT &= 0b00000000;
         B22--;
        }
 
