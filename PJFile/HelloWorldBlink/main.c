831e5fdc256881145184ca191f65c719966b4ea8
NguyenLuongDuyKhanh <16520584@gm.uit.edu.vn>
2019-04-07 12:10:22 +0700

    lab2

--- PJFile/HelloWorldBlink/main.c
+++ PJFile/HelloWorldBlink/main.c
@@ -1,48 +1,119 @@
 #include "msp430g2553.h"  //Contains all definitions for registers and built-in functions
-#define delayCircleNum 30000
+#include <ctype.h>
+#include <stdlib.h>
+//#include "morse.h"
 
-void delay(int n);
+#define delayperSig  300000
+#define delayperChar 600000
+#define delayperWord 1000000
+
+//#define delayperSig  1
+//#define delayperChar 1
+//#define delayperWord 1
+
+#define setLedRed P1OUT|=(1<<0)
+#define clrLedRed P1OUT&=~(1<<0)
+#define setLedGreen P1OUT|=(1<<6)
+#define clrLedGreen P1OUT&=~(1<<6)
+
+//void delay(int n);
+
+char name[]={'N','G','U','Y','E','N',' ','L','U','O','N','G',' ','D','U','Y',' ','K','H','A','N','H'};
+
+//char name[]={'A',' ','B',' ','C'};
+unsigned int morse[][6]=
+    {   //col 0->6
+        { 2, 1, 0, 0, 0, 0}, //A
+        { 4, 0, 1, 1, 1, 0}, //B
+        { 4, 0, 1, 0, 1, 0}, //C
+        { 3, 0, 1, 1, 0, 0}, //D
+        { 1, 1, 0, 0, 0, 0}, //E
+        { 4, 1, 1, 0, 1, 0}, //F
+        { 3, 0, 0, 1, 0, 0}, //G
+        { 4, 1, 1, 1, 1, 0}, //H
+        { 2, 1, 1, 0, 0, 0}, //I
+        { 4, 1, 0, 0, 0, 0}, //J
+        { 3, 0, 1, 0, 0, 0}, //K
+        { 4, 1, 0, 1, 1, 0}, //L
+        { 2, 0, 0, 0, 0, 0}, //M
+        { 2, 0, 1, 0, 0, 0}, //N
+        { 3, 0, 0, 0, 0, 0}, //O
+        { 4, 1, 0, 0, 1, 0}, //P
+        { 4, 0, 0, 1, 0, 0}, //Q
+        { 3, 1, 0, 1, 0, 0}, //R
+        { 3, 1, 1, 1, 0, 0}, //S
+        { 1, 0, 0, 0, 0, 0}, //T
+        { 3, 1, 1, 0, 0, 0}, //U
+        { 4, 1, 1, 1, 0, 0}, //V
+        { 3, 1, 0, 0, 0, 0}, //W
+        { 4, 0, 1, 1, 0, 0}, //X
+        { 4, 0, 1, 0, 0, 0}, //Y
+        { 4, 0, 0, 1, 1, 0}, //Z
+    };
 
 int main(void)  //Main program
 {
+
    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
-   P1DIR |= 0b01000001; // Set P1.0 to output and P1.3 to input direction
+   P1DIR |= 0b01000001; // Set P1.0, P1.6 to output and P1.3 to input direction
+   //Test Led
+   setLedRed;
+   setLedGreen;
+   clrLedRed;
+   clrLedGreen;
+   ///////////
 
-   while(1) //Loop forever, we'll do our job in the interrupt routine...
+ //  while(1) //Loop forever, we'll do our job in the interrupt routine...
    {
-       int LoopB1=2;
 
-      while(LoopB1!=0)
-      {
-       int B11=delayCircleNum;
-       while(B11!=0)
+       int charNumofName;
+       for (charNumofName = 0 ; charNumofName < sizeof(name) ; charNumofName++)
        {
-       P1OUT |= 0b00000001;
-       B11--;
-       }
+           if(name[charNumofName]==' ')
+                         {
+                             int delay=delayperWord;
+                             while(delay!=0)
+                             {
+                                 setLedGreen;
+                                 delay--;
+                             }
+                             clrLedGreen;
+                         }
+           else
+           {
+           int sigNum;
+           for (sigNum=0 ; sigNum < morse[name[charNumofName]-65][0]; sigNum++ )
+           {
 
-       int B12=delayCircleNum;
-       while(B12!=0)
-       {
-       P1OUT &= 0b00000000;
-       B12--;
-       }
-       LoopB1--;
-      }
+               if (morse[name[charNumofName]-65][sigNum+1] == 1)
+               {
+                   int delay=delayperSig;
+                   while(delay!=0)
+                   {
+                   setLedRed;
+                   delay--;
+                   }
+               }
+               if (morse[name[charNumofName]-65][sigNum+1] == 0)
+               {
 
-       int B21=delayCircleNum;
-       while(B21!=0)
-       {
-       P1OUT |= 0b01000000;
-       B21--;
-       }
+                   int delay=delayperSig;
+                   while(delay!=0)
+                   {
+                       clrLedRed;
+                       delay--;
+                   }
+               }
+           }
+               int delay=delayperChar;
+               while(delay!=0)
+               {
+                   clrLedRed;
+                   clrLedGreen;
+                   delay--;
+               }
 
-       int B22=delayCircleNum;
-       while(B22!=0)
-       {
-        P1OUT &= 0b00000000;
-        B22--;
+           }
        }
-
    }
 }
---
+++ PJFile/HelloWorldBlink/morse.h
@@ -0,0 +1,55 @@
+/*
+ * morse.h
+ *
+ *  Created on: Apr 7, 2019
+ *      Author: Khanh
+ */
+
+#ifndef MORSE_H_
+#define MORSE_H_
+
+
+static const char* CHAR_TO_MORSE[128] = {
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, "-.-.--", ".-..-.", NULL, NULL, NULL, NULL, ".----.",
+        "-.--.", "-.--.-", NULL, NULL, "--..--", "-....-", ".-.-.-", "-..-.",
+        "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...",
+        "---..", "----.", "---...", NULL, NULL, "-...-", NULL, "..--..",
+        ".--.-.", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+        "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+        ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--",
+        "-..-", "-.--", "--..", NULL, NULL, NULL, NULL, "..--.-",
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
+
+static const char* MORSE_TO_CHAR[128] = {
+        NULL, NULL, "E", "T", "I", "N", "A", "M",
+        "S", "D", "R", "G", "U", "K", "W", "O",
+        "H", "B", "L", "Z", "F", "C", "P", NULL,
+        "V", "X", NULL, "Q", NULL, "Y", "J", NULL,
+        "5", "6", NULL, "7", NULL, NULL, NULL, "8",
+        NULL, "/", NULL, NULL, NULL, "(", NULL, "9",
+        "4", "=", NULL, NULL, NULL, NULL, NULL, NULL,
+        "3", NULL, NULL, NULL, "2", NULL, "1", "0",
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, ":",
+        NULL, NULL, NULL, NULL, "?", NULL, NULL, NULL,
+        NULL, NULL, "\"", NULL, NULL, NULL, "@", NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, "'", NULL,
+        NULL, "-", NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, ".", NULL, "_", ")", NULL, NULL,
+        NULL, NULL, NULL, ",", NULL, "!", NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+};
+
+const char* char_to_morse(char);
+const char* morse_to_char(const char*);
+int morse_to_index (const char*);
+
+
+#endif /* MORSE_H_ */
