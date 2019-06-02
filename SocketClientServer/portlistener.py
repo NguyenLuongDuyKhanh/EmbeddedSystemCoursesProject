b0e00e3fea9744f87e03f469c4ee38cc26d3e375
NguyenLuongDuyKhanh <16520584@gm.uit.edu.vn>
2019-06-02 09:56:13 +0000

    xong json

--- SocketClientServer/portlistener.py
+++ SocketClientServer/portlistener.py
@@ -1,6 +1,10 @@
 import json
 from pymongo import MongoClient
 import socket
+import bson
+import collections
+from bson.codec_options import CodecOptions
+from bson.json_util import dumps
 
 print ('connect successful!!')
 
@@ -15,19 +19,42 @@ print ('socket binded to {0:d}'.format(port) )
 
 s.listen(5)
 print ('socket is listening')
+
 client = MongoClient()
 db = client.StorgeTempHum
 coll = db.realTimeupdate
-#col = coll.find_one()
-#print(col)
-#print (coll)
 
+#col = coll.find_one()
+
+"""
 while True:  
    c, addr = s.accept()      
    print ('Got connection from', addr) 
  
-  # c.send('Thank you for connecting') 
+   c.sendall('Thank you for connecting') 
+   
+   content = c.recv(1024)
+   print(content)
+
    for x in coll.find():
     print(x)
    c.close() 
-
+"""
+conn, addr = s.accept()
+with conn:
+  print('Connected by', addr)
+  while True:
+     data = conn.recv(1024)
+     #jsonObj = json.loads(coll.find_one())
+     if not data:
+        break
+     print(data.decode())
+     a=coll.find_one()
+     #print(a)
+     #b=bson.BSON.encode(a)
+     datatosent = dumps(db.realTimeupdate.find_one())
+     #print (dumps(db.realTimeupdate.find_one()))
+     #print(datatosent.encode('utf-8'))
+     #print(datatosent)
+     conn.sendall(datatosent.encode('utf-8'))
+    # conn.sendall(b)
